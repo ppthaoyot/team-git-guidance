9ddc54fbcf086eb04c629f6ee6484f6a9023f0a0
ppthaoyot <57580564+ppthaoyot@users.noreply.github.com>
2026-06-17 09:23:51 +0000

    Deploying to gh-pages from @ ppthaoyot/team-git-guidance@c6876a681a6ead94a2322a77eca5be5ea430ef8e 🚀

---
+++ configuration.js
@@ -0,0 +1,26 @@
+window.__CONST__ENV__ = {
+  "VITE_APP_NAME": "ค้นหาบัตรประกัน PA นักเรียน",
+  "VITE_APP_VERSION": "1.2.0",
+  "VITE_APP_SINCE": "2026",
+  "VITE_APP_DESCRIPTION": "ระบบค้นหาและแสดงบัตรประกันภัยอุบัติเหตุส่วนบุคคล (PA) สำหรับนักเรียน",
+  "VITE_APP_CONTACT_URL": "https://www.siamsmile.co.th",
+  "VITE_VERSION_CHECKER_CONFIRM": "true",
+  "VITE_VERSION_CHECKER_CHECK_INTEVAL_MIN": "15",
+  "VITE_BASE_URL": "/pa-student-card/",
+  "VITE_API_URL": "https://demoposapi.siamsmile.co.th",
+  "VITE_APIGW_BASEURL": "https://apigw.siamsmile.co.th",
+  "VITE_SSO_ISSUER": "https://oauthlogin.siamsmile.co.th",
+  "VITE_SSO_CLIENT_ID": "835badca-58b1-451a-8fb3-05d2915081ba",
+  "VITE_SSO_SCOPE": "openid profile roles email employee_profile employee_team employee_position employee_department employee_branch queue_api",
+  "VITE_DOCSTORAGE_URL": "https://docstorage.siamsmile.co.th",
+  "VITE_QUEUE_URL": "https://queue.uatsiamsmile.com",
+  "VITE_QUEUE_API_URL": "https://queueapi.uatsiamsmile.com/api",
+  "VITE_QUEUE_REFETCH_INTERVAL": "30",
+  "VITE_OUTPUT_DIR": "dist",
+  "MODE": "production"
+};
+Object.freeze(window.__CONST__ENV__);
+Object.defineProperty(window, "__CONST__ENV__", {
+    configurable: false,
+    writable: false,
+});
